aab48b05b468f1443be71354d3d246c35cc82040
Valeriy Gorshevskiy <gorshchevskiy90@mail.ru>
2023-09-16 11:19:16 +0300

    доделал ДЗ_1

--- src/channel.py
+++ src/channel.py
@@ -3,10 +3,11 @@ import os
 import requests
 from googleapiclient.discovery import build
 
-api_key: str = os.getenv('API_KEY')
+
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
 
