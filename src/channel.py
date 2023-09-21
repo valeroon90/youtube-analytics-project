673512fc9a064970d91728a9ba896bdcbd78d89e
Valeriy Gorshevskiy <gorshchevskiy90@mail.ru>
2023-09-21 21:52:48 +0300

    Окончил ДЗ_2

--- src/channel.py
+++ src/channel.py
@@ -14,6 +14,17 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         self.channel_id = channel_id
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.info = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.info["items"][0]["snippet"]["title"]
+        self.description = self.info["items"][0]["snippet"]["description"]
+        self.url = self.info["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        self.subscriberCount = self.info["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.info["items"][0]["statistics"]["videoCount"]
+        self.viewCount = self.info["items"][0]["statistics"]["viewCount"]
+
+
+
+
 
 
     def print_info(self) -> None:
@@ -21,3 +32,22 @@ class Channel:
         channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
+
+
+    def to_json(self, filename):
+        result = {
+            'id': self.__channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriberCount': self.subscriberCount,
+            'video_count': self.video_count,
+            'viewCount': self.viewCount
+        }
+        file = open(filename, "w", encoding='utf-8')
+        json.dump(result, file)
+        file.close()
+
+    @staticmethod
+    def get_service():
+        return Channel.youtube
